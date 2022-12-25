8e555a70bdba0defd1574aabf1aacc1fe41b32b4
Sabır Akbana <79760891+sbrakb@users.noreply.github.com>
2022-12-25 17:59:15 +0300

    Add files via upload

---
+++ console-programlama-9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace console_programlama_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Lütfen Gidilecek Sayıyı giriniz !");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam =0;
+            while (sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayac);
+
+            // a dan z ye yazdır 
+            char character ='a';
+            while (character<'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+            Console.WriteLine("******Foreach******");
+            string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+                
+            }
+        }
+    }
+}
